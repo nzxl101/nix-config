eb4fdffdcf1c9e08db8c3711f16ad0ca6ab8682b
nzxl101 <yyy.noel.yyy@gmail.com>
2025-04-18 02:24:03 +0200

    ...

--- home/nzxl/modules/zsh.nix
+++ home/nzxl/modules/zsh.nix
@@ -36,7 +36,7 @@
         # Set rgb colors for laptop
         if [ "$(hostname)" = "shin" ]; then
           exec polychromatic-cli -d laptop -z main -o static -c FF0000
-          exec polychromatic-cli -d laptop -z main -o static -p 75
+          exec polychromatic-cli -d laptop -z main -o brightness -p 75
           exec polychromatic-cli -d laptop -z logo -o brightness -p 1
         fi
       fi
