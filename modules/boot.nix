4371abd43d6bf730a23671fb59300dd3e83863c6
nzxl101 <yyy.noel.yyy@gmail.com>
2025-04-19 08:04:04 +0200

    ...

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,6 +1,18 @@
 {
+  boot.consoleLogLevel = 3;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
   boot.initrd.kernelModules = [];
-  boot.kernelParams = [];
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+  ];
+  boot.plymouth = {
+    enable = true;
+  };
 }
