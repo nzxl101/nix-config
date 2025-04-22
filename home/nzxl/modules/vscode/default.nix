410bf38772794f00a57e70515e9bffbd1c614b58
nzxl101 <yyy.noel.yyy@gmail.com>
2025-04-22 14:34:50 +0200

    ...

--- home/nzxl/modules/vscode/default.nix
+++ home/nzxl/modules/vscode/default.nix
@@ -3,7 +3,7 @@
     enable = true;
   };
 
-  home.file."~/.config/Code/User/settings.json".source = lib.mkForce (
-    config.lib.file.mkOutOfStoreSymlink "~/nix-config/home/${primaryUser}/modules/vscode/settings.json"
+  home.file."${config.home.homeDirectory}/.config/Code/User/settings.json".source = lib.mkForce (
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/${primaryUser}/modules/vscode/settings.json"
   );
 }
