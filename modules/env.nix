8b2681779b51d5e24ca9016abc6473fbd8eb2d74
nzxl101 <yyy.noel.yyy@gmail.com>
2025-04-27 13:27:04 +0200

    ...

--- modules/env.nix
+++ modules/env.nix
@@ -7,5 +7,8 @@
       "${XDG_BIN_HOME}"
       "$HOME/nix-config/scripts"
     ];
+    MESA_SHADER_CACHE_DIR = "$HOME/.cache/mesa_shader_cache";
+    MESA_SHADER_CACHE_MAX_SIZE = "1G";
+    MESA_SHADER_CACHE_DISABLE = "0";
   };
 }
