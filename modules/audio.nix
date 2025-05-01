c95f496aaafcc73cb3db2aa7dd259f8a2ba19c60
nzxl101 <yyy.noel.yyy@gmail.com>
2025-05-01 07:38:05 +0200

    ...

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,4 +1,4 @@
-{
+{ config, lib, ... }: {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -6,23 +6,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
 
-    # Low-latency
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 384;
-        "default.clock.min-quantum" = 384;
-        "default.clock.max-quantum" = 384;
-      };
-    };
-
     # Bluetooth Audio
-    wireplumber.extraConfig.bluetoothEnhancements = {
-      "monitor.bluez.properties" = {
+    wireplumber.extraConfig = lib.mkIf config.hardware.bluetooth.enable {
+      bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
           "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        };
       };
     };
   };
