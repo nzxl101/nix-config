a082aedbc2364866ba8d57e146a3731edbe42bc8
nzxl101 <yyy.noel.yyy@gmail.com>
2025-05-19 03:28:11 +0200

    ...

--- hosts/lynx/configuration.nix
+++ hosts/lynx/configuration.nix
@@ -7,12 +7,10 @@
   system.stateVersion = stateVersion;
 
   # Static IP configuration
-  # networking.interfaces.eth0.ipv4.addresses = [{
-  #   address = "192.168.178.101";
-  #   prefixLength = 24;
-  # }];
-  # networking.defaultGateway = "192.168.178.1";
-  # networking.nameservers = [ "192.168.178.1" ];
+  networking.interfaces.ens18.ipv4.addresses = [{
+    address = "192.168.178.108";
+    prefixLength = 24;
+  }];
   networking.firewall.allowedTCPPorts = [ 22 ];
 
   # SSH
